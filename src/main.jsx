b595afa14868e3461625e4843966173f225c6672
Khaydann <aidaney02@gmail.com>
2024-10-22 01:07:39 +0300

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import App from './App.jsx'
 // import './index.css'
-import Footer from './assets/Sual/Sikayet.jsx'
-import Sikayet from './assets/Sual/Sikayet.jsx'
+import Footer from './assets/Sikayet/Sikayet.jsx'
+import Sikayet from './assets/Sikayet/Sikayet.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
