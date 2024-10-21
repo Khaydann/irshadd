040d1e2f2f301a6d0cf00657f1afc7dcb49be185
Khaydann <aidaney02@gmail.com>
2024-10-21 23:52:14 +0300

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import App from './App.jsx'
 // import './index.css'
-import Footer from './assets/Footer/Footer.jsx'
+import Footer from './assets/Sual/Sual.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
